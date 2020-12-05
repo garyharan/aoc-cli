51ef39eafe5cd941e033f16477e945a15a2be890
Gary Haran <gary.haran@shopify.com>
2020-12-05 08:26:56 -0500

    day 5 refactor

--- src/year2020/day05.rb
+++ src/year2020/day05.rb
@@ -1,19 +1,25 @@
 module Year2020
   class Day05
     def part1(input)
-      input.map do |code|
-        RowPosition.new(code).to_i * 8 + ColumnPosition.new(code).to_i
-      end.max
+      input.map { |code| Seat.new(code).id }.max
     end
 
     def part2(input)
-      ids = input.map do |code|
-        RowPosition.new(code).to_i * 8 + ColumnPosition.new(code).to_i
-      end
+      ids = input.map { |code| Seat.new(code).id }
 
       min, max = ids.minmax
 
       (min..max).inject(&:+) - ids.inject(&:+)
+    end
+  end
+
+  class Seat
+    def initialize(code)
+      @code = code
+    end
+
+    def id
+      RowPosition.new(@code).to_i * 8 + ColumnPosition.new(@code).to_i
     end
   end
 
