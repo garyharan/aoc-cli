823848c335efe98fb78dabaaab9e06c049a0c32e
Gary Haran <gary.haran@shopify.com>
2020-12-05 08:18:23 -0500

    day 5 improvements

--- src/year2020/day05.rb
+++ src/year2020/day05.rb
@@ -26,38 +26,20 @@ module Year2020
   class RowPosition
     def initialize(code)
       @code = code.slice(0, 7)
-      @rows = (0..127).to_a
     end
 
     def to_i
-      @code.chars.reduce(@rows) do |rows, char|
-        if char == 'F'
-          rows.slice!((rows.length - rows.length / 2), rows.length)
-        elsif char == 'B'
-          rows.slice!(0, (rows.length - rows.length / 2))
-        end
-
-        rows
-      end.first
+      @code.tr("BF", "10").to_i(2)
     end
   end
 
   class ColumnPosition
     def initialize(code)
       @code = code.slice(7, 3)
-      @cols = (0..7).to_a
     end
 
     def to_i
-      @code.chars.reduce(@cols) do |rows, char|
-        if char == 'L'
-          rows.slice!((rows.length - rows.length / 2), rows.length)
-        elsif char == 'R'
-          rows.slice!(0, (rows.length - rows.length / 2))
-        end
-
-        rows
-      end.first
+      @code.tr("RL", "10").to_i(2)
     end
   end
 end
